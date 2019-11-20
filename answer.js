f17d904c441a8f5a0eeb26e78b8b5b3e43788d33
reeganford <rmford1695@gmail.com>
2019-11-20 16:51:46 -0700

    Finished challenge #6

--- answer.js
+++ answer.js
@@ -1,1 +1,15 @@
 // *** YOUR ANSWER BELOW ***
+
+// Converting pounds to number of jellybeans
+
+const AVERAGE_JELLYBEAN_WEIGHT_GRAMS = 1.18;
+const GRAMS_PER_POUND = 453.59;
+
+function howManyJellybeans(pounds){
+    let poundsOfJellybeans = AVERAGE_JELLYBEAN_WEIGHT_GRAMS / GRAMS_PER_POUND;
+    let totalJellybeans = pounds / poundsOfJellybeans;
+    return Math.floor(totalJellybeans);
+
+}
+
+console.log(howManyJellybeans(100));
